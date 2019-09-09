9c0005b2e22bc0c6273aad570fb036165acd27f2
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-09 11:39:34 +0300

    Add files via upload

---
+++ Multidimensional Arrays-Exercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var side = int.Parse(Console.ReadLine());
+            var matrix = new int[side, side];
+            for (int row = 0; row < side; row++)
+            {
+                var rowInput = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < side; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+            var primaryDiagonal = 0;
+            var secondaryDiagonal = 0;
+            for (int row = 0; row < side; row++)
+            {
+                for (int col = 0; col < side; col++)
+                {
+                    if (row == col)
+                    {
+                        primaryDiagonal += matrix[row, col];
+                    }
+                    if (col + row == side - 1)
+                    {
+                        secondaryDiagonal += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+        }
+    }
+}
