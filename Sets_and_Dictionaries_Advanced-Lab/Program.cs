c2e63d9336625a2707abac480a6d9fd9ecd45611
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-10 15:00:51 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = Console
+                .ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            var numbersDict = new Dictionary<double, int>();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (!numbersDict.ContainsKey(inputArray[i]))
+                {
+                    numbersDict.Add(inputArray[i], 0);
+                }
+                numbersDict[inputArray[i]]++;
+            }
+            foreach (var num in numbersDict)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
