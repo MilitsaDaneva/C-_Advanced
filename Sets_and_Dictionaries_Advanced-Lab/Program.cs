75468804dd55e6fbfcee1d81a606eae4b242e9be
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-10 15:08:44 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesNum = int.Parse(Console.ReadLine());
+            var namesCollection = new HashSet<string>();
+
+            for (int i = 0; i < namesNum; i++)
+            {
+                var currentName = Console.ReadLine();
+                namesCollection.Add(currentName);                    
+            }
+
+            foreach (var name in namesCollection)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
