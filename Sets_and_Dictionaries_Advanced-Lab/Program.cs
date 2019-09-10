afb4617afe71827fecd15ae94ee327c9e0783711
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-10 15:02:50 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Lab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shopDict = new SortedDictionary<string, Dictionary<string, double>>();
+            var shopInputInfo = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (shopInputInfo[0] != "Revision")
+            {
+                var shop = shopInputInfo[0];
+                var product = shopInputInfo[1];
+                var price = double.Parse(shopInputInfo[2]);
+
+                if (!shopDict.ContainsKey(shop))
+                {
+                    shopDict.Add(shop, new Dictionary<string, double>());
+                }
+                if (!shopDict[shop].ContainsKey(product))
+                {
+                    shopDict[shop].Add(product, 0.00);
+                }
+                shopDict[shop][product] = price;
+
+                shopInputInfo = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var shop in shopDict)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
