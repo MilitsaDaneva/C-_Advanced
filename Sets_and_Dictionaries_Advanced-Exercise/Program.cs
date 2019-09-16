fe821e557784eae55225d1d1d5ac21f333c0847f
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-16 17:05:09 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console
+                .ReadLine()
+                .ToCharArray();
+
+            var charDictionary = new SortedDictionary<char, int>();
+            foreach (var symbol in inputString)
+            {
+                if (!charDictionary.ContainsKey(symbol))
+                {
+                    charDictionary.Add(symbol, 0);
+                }
+                charDictionary[symbol]++;
+            }
+            foreach (var kvp in charDictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
