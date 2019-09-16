5db953e801ecf3dd141b59fdf25271e8cd9d5db1
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-16 16:50:42 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userNames = new HashSet<string>();
+            var userNum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < userNum; i++)
+            {
+                var newUser = Console.ReadLine();
+                userNames.Add(newUser);
+            }
+            foreach (var user in userNames)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
