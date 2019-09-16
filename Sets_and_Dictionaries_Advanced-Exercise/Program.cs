34d1c486307198bf2eed94b021f3a8d77f097c19
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-16 17:03:05 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chemicalEllements = new SortedSet<string>();
+            var numberInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInputs; i++)
+            {
+                var chemEllementsArray = Console
+                    .ReadLine()
+                    .Split();
+                for (int x = 0; x < chemEllementsArray.Length; x++)
+                {
+                    chemicalEllements.Add(chemEllementsArray[x]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemicalEllements));
+        }
+    }
+}
