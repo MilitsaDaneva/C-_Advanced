2713cdf18c3a6494fc492b3318b1a8a3db138350
MilitsaDaneva <53004565+MilitsaDaneva@users.noreply.github.com>
2019-09-16 17:03:59 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersAppearance = new Dictionary<int, int>();
+            var numberInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInputs; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                if (!numbersAppearance.ContainsKey(currentNum))
+                {
+                    numbersAppearance.Add(currentNum, 0);
+                }
+                numbersAppearance[currentNum]++;
+            }
+            foreach (var kvp in numbersAppearance)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
